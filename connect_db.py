de59c400d94f9a14b014b528373b1fe4ea3ae427
Savvy325 <vannahlyles@yahoo.com>
2024-04-13 18:59:52 -0500

    first commit

---
+++ connect_db.py
@@ -0,0 +1,25 @@
+import mysql.connector
+from mysql.connector import Error
+
+def connect_db():
+    db_name = "library_management"
+    user = "root"
+    password = "Savannah!1015"
+    host = "localhost"
+
+    try: 
+        conn = mysql.connector.connect(
+            database = db_name,
+            user = user,
+            password=password,
+            host=host        
+        )
+
+        return conn 
+
+
+    
+    except mysql.connector.Error as e:
+        print(f'\nAn error {e} has occurred...please try again ┌( ಠ_ಠ)┘!')
+
+# connect_db()
